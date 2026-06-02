f7c0561f44c44f8a9d8e86fd572c6ac22332f244
Anuj <anujpal2454@gmail.com>
2026-06-02 23:01:10 +0530

    consumer added

---
+++ src/main/java/com/pag/order_service/OrderConsumer.java
@@ -0,0 +1,17 @@
+package com.pag.order_service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    @KafkaListener(
+            topics = "order-created",
+            groupId = "order-group")
+    public void consume(String message) {
+
+        System.out.println(
+                "Received Message => " + message);
+    }
+}
